6b14deeec5f38b0c88671048c8aa1c308d75bbaf
Alejandro Lopez-tormos <ale.tormos@gmail.com>
2026-02-25 13:22:43 +0100

    Feat: codigo hecho en clase

---
+++ Practica2/SegurosBusiness/src/main/java/GestionSeguros.java
@@ -0,0 +1,5 @@
+package es.unican.is2.SegurosBusiness;
+
+public class GestionSeguros implements IGestionSeguros {
+	
+}
---
+++ Practica2/SegurosBusiness/src/main/java/es/unican/is2/SegurosBusiness/App.java
@@ -0,0 +1,7 @@
+package es.unican.is2.SegurosBusiness;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
