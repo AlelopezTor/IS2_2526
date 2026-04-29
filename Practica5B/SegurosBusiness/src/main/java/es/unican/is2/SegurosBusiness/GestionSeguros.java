9a88fadd846401c31ea1560360630a75c3a09321
Alejandro Lopez-tormos <ale.tormos@gmail.com>
2026-04-29 12:57:56 +0200

    Inicio

---
+++ Practica5B/SegurosBusiness/src/main/java/es/unican/is2/SegurosBusiness/GestionSeguros.java
@@ -0,0 +1,121 @@
+package es.unican.is2.SegurosBusiness;
+import es.unican.is2.SegurosCommon.Seguro;
+import es.unican.is2.SegurosCommon.IGestionSeguros;
+import es.unican.is2.SegurosCommon.IInfoSeguros;
+import es.unican.is2.SegurosCommon.ISegurosDAO;
+import es.unican.is2.SegurosCommon.Cliente;
+import es.unican.is2.SegurosCommon.DataAccessException;
+import es.unican.is2.SegurosCommon.IClientesDAO;
+import es.unican.is2.SegurosCommon.IGestionClientes;
+import es.unican.is2.SegurosCommon.OperacionNoValida;
+
+
+
+public class GestionSeguros implements IGestionClientes, IGestionSeguros, IInfoSeguros {
+
+	private ISegurosDAO segurosDao;
+	private IClientesDAO clientesDao;
+	
+
+	
+	public GestionSeguros(IClientesDAO daoClientes, ISegurosDAO daoSeguros) {
+		this.segurosDao = daoSeguros;
+		this.clientesDao = daoClientes;
+		}
+
+	/**
+	 * Agrega un nuevo seguro al cliente cuyo dni se indica.
+	 * @param s Seguro que desea agregar
+	 * @param dni DNI del cliente
+	 * @return El seguro agregado
+	 * 		   null si no se agrega porque no se encuentra el cliente
+	 * @throws OperacionNoValida si el seguro ya existe
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+
+	public Seguro nuevoSeguro(Seguro s, String dni) throws OperacionNoValida, DataAccessException {
+		Cliente cliente = clientesDao.cliente(dni);
+		if (cliente == null) {
+			return null;
+		}
+		Seguro se = segurosDao.creaSeguro(s);
+		if (se == null) {
+			throw new OperacionNoValida("El seguro ya existe");
+		}
+		cliente.getSeguros().add(se);
+		clientesDao.actualizaCliente(cliente);
+		return se;
+	}
+	
+	/**
+	 * Elimina el seguro cuya matricula se indica y 
+	 * que pertenece al cliente cuyo dni se indica
+	 * @param matricula Identificador del seguro a eliminar
+	 * @param dni DNI del propietario del seguro
+ 	 * @return El seguro eliminado
+ 	 *         null si el seguro o el cliente no existen
+ 	 * @throws OperacionNoValida si el seguro no pertenece al dni indicado
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+		
+	public Seguro bajaSeguro(String matricula, String dni) throws OperacionNoValida, DataAccessException{
+		Seguro seguro = segurosDao.seguroPorMatricula(matricula);
+		Cliente cliente = clientesDao.cliente(dni);
+
+		if (seguro == null || cliente == null) {
+			return null;
+		}
+		if (cliente.getSeguros().contains(seguro) == false) {
+			throw new OperacionNoValida("No se puede dar de baja el seguro con este id");
+		}
+		cliente.getSeguros().remove(seguro);
+		seguro = segurosDao.eliminaSeguro(seguro.getId());
+		clientesDao.actualizaCliente(cliente);
+		return seguro;
+}
+	/**
+	 * Agrega o modifica el conductor adicional al seguro cuya matricula se indica
+	 * @param matricula Identificador del seguro
+	 * @param conductor Nombre del conductor adicional a agregar
+ 	 * @return El seguro modificado
+ 	 *         null si el seguro no existe
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+	public Seguro anhadeConductorAdicional(String matricula, String conductor) throws DataAccessException{
+		Seguro s = segurosDao.seguroPorMatricula(matricula);
+		if (s == null) {
+			return null;
+		}
+		s.setConductorAdicional(conductor);
+		return segurosDao.actualizaSeguro(s);
+		
+	}
+	public Cliente nuevoCliente(Cliente c) throws DataAccessException{
+		return clientesDao.creaCliente(c);
+	}
+	public Cliente bajaCliente(String dni) throws OperacionNoValida, DataAccessException {
+	    Cliente c = clientesDao.cliente(dni);
+	    if (c == null) {
+	        return null;
+	    }
+	    if (!c.getSeguros().isEmpty()) {
+	        throw new OperacionNoValida("El cliente tiene seguros");
+	    }
+	    return clientesDao.eliminaCliente(dni);
+	}
+	@Override
+	public Cliente cliente (String dni)  throws DataAccessException{
+		Cliente c = clientesDao.cliente(dni);
+		return c;
+	}
+	@Override
+	public Seguro seguro (String matricula)  throws DataAccessException{
+		
+		Seguro s = segurosDao.seguroPorMatricula(matricula);
+		return s;
+	}
+	
+}
---
+++ Practica5B/SegurosCommon/src/test/java/es/unican/is2/SegurosCommon/ClienteTest.java
@@ -0,0 +1,96 @@
+package es.unican.is2.SegurosCommon;
+
+import static org.junit.jupiter.api.Assertions.*;
+ 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+ 
+public class ClienteTest {
+    private Cliente clienteConMinusvalia;
+    private Cliente clienteSinMinusvalia;
+ 
+    private Seguro s1;
+    private Seguro s2;
+    private Seguro s3;
+    private Seguro s4;
+ 
+    @BeforeEach
+    public void setUp() {
+        clienteConMinusvalia  = new Cliente("12345678B", "Alejandro", true);
+        clienteSinMinusvalia  = new Cliente("87654321Z", "Pepe", false);
+ 
+        s1= new Seguro("1111AAA", 0, Cobertura.TODO_RIESGO, LocalDate.now());
+        s2 = new Seguro("1111BBB", 44, Cobertura.TERCEROS_LUNAS, LocalDate.now());
+        s3 = new Seguro("1111CCC", 105, Cobertura.TERCEROS, LocalDate.now());
+        s4 = new Seguro("2222AAA", 44, Cobertura.TERCEROS_LUNAS, LocalDate.now());
+    }
+
+    @Test
+    public void testConstructor() {
+ 
+        Cliente clienteMinusvalía  = new Cliente("12345678B", "Alejandro", true);
+        assertEquals("12345678B", clienteMinusvalía.getDni());
+        assertTrue(clienteMinusvalía.getMinusvalia());
+        Cliente clienteSinMinusvalia  = new Cliente("12345678B", "Alejandro", false);
+        assertFalse(clienteSinMinusvalia.getMinusvalia());
+ 
+        assertThrows(IllegalArgumentException.class, () -> new Cliente("aaaa1234", "Alejandro", true));
+        assertThrows(IllegalArgumentException.class, () -> new Cliente("1111111111", "Alejandro", true));
+ 
+        assertThrows(NullPointerException.class, () -> new Cliente(null, "Ana", true));
+        assertThrows(NullPointerException.class, () -> new Cliente("12345678B", null, true));
+        
+        new Cliente();
+    }
+ 
+    @Test
+    public void testTotalSeguros() {
+
+        List<Seguro> tresSegurosMinus = Arrays.asList(s1, s2, s3);
+        clienteConMinusvalia.setSeguros(tresSegurosMinus);
+        assertEquals(1212, clienteConMinusvalia.totalSeguros());
+        List<Seguro> unSeguro = Arrays.asList(s4);
+        clienteSinMinusvalia.setSeguros(unSeguro);
+        assertEquals(480, clienteSinMinusvalia.totalSeguros());
+ 
+        Cliente clienteFallo = new Cliente("12345678E", "Alejandro", true);
+        assertThrows(IllegalArgumentException.class, () -> clienteFallo.totalSeguros());
+ 
+        List<Seguro> conNull = Arrays.asList(s1, null, s3);
+        clienteFallo.setSeguros(conNull);
+        assertThrows(NullPointerException.class, () -> clienteFallo.totalSeguros());
+        
+        clienteFallo.setSeguros(null);
+        assertThrows(NullPointerException.class, () -> clienteFallo.totalSeguros());
+    }
+    
+    @SuppressWarnings("unlikely-arg-type")
+	@Test
+    public void testEquals() {
+        Cliente mismoDni = new Cliente("12345678B", "Otro nombre", false);
+        assertTrue(clienteConMinusvalia.equals(mismoDni));
+        assertFalse(clienteConMinusvalia.equals("aaaaaa"));
+        Cliente distintoDni = new Cliente("87654321Z", "Alejandro", true);
+        assertFalse(clienteConMinusvalia.equals(distintoDni));
+        assertFalse(clienteConMinusvalia.equals(null));
+        //Cliente nullDni = new Cliente(null, "Alejandro", true); Da excepción y no llega a más
+        //assertFalse(clienteConMinusvalia.equals(nullDni));
+        
+        assertTrue(clienteConMinusvalia.equals(clienteConMinusvalia));
+
+    }
+ 
+    @Test
+    public void testHashCode() { 
+        Cliente c1 = new Cliente("12345678B", "Alejandro", true);
+        Cliente c2 = new Cliente("12345678B", "Otro",      false);
+        assertEquals(c1.hashCode(), c2.hashCode());
+ 
+        Cliente c3 = new Cliente("87654321Z", "Pepe", false);
+        assertNotEquals(c1.hashCode(), c3.hashCode());
+    }
+    
+}
